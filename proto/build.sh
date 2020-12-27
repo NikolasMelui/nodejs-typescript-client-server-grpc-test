198b08f80d81d8d11d4a68291193e45b53322ee7
nikolasmelui <slak@samaradom.ru>
2020-12-27 22:47:32 +0300

    Fix the build.sh script

--- proto/build.sh
+++ proto/build.sh
@@ -2,16 +2,16 @@
 
 PROTO_DIR=./proto
 
-# Geerate JavaScript code 
-npm run grpc_tools_node_protoc \
+# Generate JavaScript code
+npx grpc_tools_node_protoc \
     --js_out=import_style=commonjs,binary:${PROTO_DIR} \
     --grpc_out=${PROTO_DIR} \
     --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
     -I ./proto \
     proto/*.proto
 
-# Geerate TypeScript code (d.ts)
-npm run grpc_tools_node_protoc \
+# Generate TypeScript code (d.ts)
+npx grpc_tools_node_protoc \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
     --ts_out=${PROTO_DIR} \
     -I ./proto \
