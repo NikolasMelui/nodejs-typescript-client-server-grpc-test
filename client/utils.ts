f4262ba789dd3219c4c63eb94477d264834602de
nikolasmelui <slak@samaradom.ru>
2021-01-05 12:16:55 +0400

    Add first utils.ts client file

---
+++ client/utils.ts
@@ -0,0 +1,8 @@
+import { UsersClient } from "../proto/users_grpc_pb";
+import { credentials } from "grpc";
+
+const port = 3000;
+
+export const client = new UsersClient(`localhost:${port}`, credentials.createInsecure());
+
+export const noop = () => {};
