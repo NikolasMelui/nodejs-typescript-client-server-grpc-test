0783f083b57d4a6230f5ecdb8210a77daa8a22ab
nikolasmelui <slak@samaradom.ru>
2021-01-17 21:34:24 +0300

    Add get-users.ts file

---
+++ client/get-users.ts
@@ -0,0 +1,12 @@
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+import { User } from "../proto/users_pb";
+import { client } from "./utils";
+
+export default () =>
+  new Promise<User[]>((resolve, reject) => {
+    const stream = client.getUsers(new Empty());
+    const users: User[] = [];
+    stream.on("data", (user) => users.push(user));
+    stream.on("error", reject);
+    stream.on("end", () => resolve(users));
+  });
